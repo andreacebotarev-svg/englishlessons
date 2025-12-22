438f4033cc508a6eb7832e9f3a6e14c4b680968f
andreacebotarev-svg <andreacebotarev@gmail.com>
2025-12-22 12:59:02 +0800

    feat: entities/dictionary - types, Zod schemas, LessonLoader with caching

---
+++ trainer/src/entities/dictionary/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Dictionary entity public API
+ */
+
+export * from './types';
+export { WordSchema, LessonSchema, LessonMetaSchema } from './schema';
+export { LessonLoader, lessonLoader } from './LessonLoader';
