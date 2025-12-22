5ff0aa9b43ab85398ec589c60a0f115540f62a24
andreacebotarev-svg <andreacebotarev@gmail.com>
2025-12-22 13:49:19 +0800

    Delete trainer directory

--- trainer/src/entities/dictionary/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Dictionary entity public API
- */
-
-export * from './types';
-export { WordSchema, LessonSchema, LessonMetaSchema } from './schema';
-export { LessonLoader, lessonLoader } from './LessonLoader';
--- trainer/src/entities/session/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Session entity public API
- */
-
-export * from './types';
-export { SessionStore, sessionStore } from './SessionStore';
